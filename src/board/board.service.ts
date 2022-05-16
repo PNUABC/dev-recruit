c3e6d139836b84735ae2a1ecc316b905357f4ae9
paul <ace95913@gmail.com>
2022-05-16 14:29:40 +0900

    feat: getBoardById
    id를 받아 board에서 해당 id를 불러옴.

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+    constructor(
+        private boardRepository:BoardRepository
+    ){}
+
+    async getBoardById(id: number){
+        const found = await this.boardRepository.findOne(id);
+        if(!found){
+            throw new NotFoundException(`Can't find board with id ${id}`);
+        }
+
+        return found;
+    }
+}
