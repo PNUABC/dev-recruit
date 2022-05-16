014c55d04c43c1db78262dfbe09a565ae38ab6d6
paul <ace95913@gmail.com>
2022-05-16 14:31:20 +0900

    feat: create board dto
    board에 대한 dto를 설계

---
+++ src/board/dto/create-board.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from "class-validator";
+
+export class CreateBoardDto{
+    @IsNotEmpty()
+    companyName: string;
+    
+    @IsNotEmpty()
+    recruitTitle: string;
+    
+    @IsNotEmpty()
+    recruitDescription: string;
+    
+    @IsNotEmpty()
+    companyAddress: string;
+    
+    @IsNotEmpty()
+    expireDate: Date;
+
+}
