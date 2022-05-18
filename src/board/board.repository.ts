cda3465ead5be88a044ef496de0a91e98892d592
paul <ace95913@gmail.com>
2022-05-18 18:29:01 +0900

    feat: BoardRepository createBoard 구현
    board dto를 통한 board 생성기능 구현

--- src/board/board.repository.ts
+++ src/board/board.repository.ts
@@ -1,6 +1,15 @@
-import { Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Board } from "./board.entity";
+import { CreateBoardDto } from "./dto/create-board.dto";
 
+@EntityRepository(Board)
 export class BoardRepository extends Repository<Board>{
-    
+    async createBoard(createBoardDto: CreateBoardDto):Promise<Board>{
+        const {companyName, recruitTitle, recruitDescription, companyAddress, expireDate} = createBoardDto;
+
+        const board = this.create({companyName, recruitTitle, recruitDescription, companyAddress, expireDate});
+
+        await this.save(board);
+        return board;
+    }
 }
