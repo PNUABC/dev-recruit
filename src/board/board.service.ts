527e8e3b60403d530e97c4b93b58faee2fa08ab9
paul <ace95913@gmail.com>
2022-05-18 18:30:50 +0900

    feat: getAllBoard 기능 구현
    db의 모든 board 가져오는 기능 구현

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { NotFoundError } from 'rxjs';
+import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardService {
     constructor(
+        @InjectRepository(BoardRepository)
         private boardRepository:BoardRepository
     ){}
 
@@ -15,5 +18,12 @@ export class BoardService {
         }
 
         return found;
+    }
+
+    async getAllBoards():Promise<Board[]>{
+        const query = this.boardRepository.createQueryBuilder('board');
+        query.where('board');
+        const boards = await query.getMany();
+        return boards;
     }
 }
