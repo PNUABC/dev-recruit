13c685a2c139eb8b27c30471537186049d03b575
paul <ace95913@gmail.com>
2022-05-18 18:30:01 +0900

    feat: BoardRepository module에 import
    생성한 board repository 다른 곳에서도 사용할 수 있게 module에 import

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   providers: [BoardService],
   controllers: [BoardController]
 })
