c361aa974157514a4fee7f7a499653c8ee67d5bf
paul <ace95913@gmail.com>
2022-08-22 19:39:28 +0900

    feat: user create 기능 구현
    user에 대한 정보 생성 기능 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+
+    // create
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto):Promise<User>{
+        return this.userService.createUser(createUserDto);
+    }
+
+    // log in
+
+}
