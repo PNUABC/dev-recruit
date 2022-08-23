d1922c1e350aad2a965a57a76cc6f42a3ab243d2
paul <ace95913@gmail.com>
2022-08-23 16:28:23 +0900

    feat: create user pipe 추가
    validation pipe 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
@@ -9,6 +9,7 @@ export class UserController {
 
     // create
     @Post()
+    @UsePipes(ValidationPipe)
     createUser(@Body() createUserDto: CreateUserDto):Promise<User>{
         return this.userService.createUser(createUserDto);
     }
