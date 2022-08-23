1d4710cf344d1637306c656d8d56cf5860af693a
paul <ace95913@gmail.com>
2022-08-23 16:28:59 +0900

    feat: user repository exception handling
    user 이름이나 이메일 중복 시 exception handling 추가

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, HttpException, HttpStatus, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./user.entity";
@@ -7,7 +8,21 @@ export class UserRepository extends Repository<User>{
     async createUser(createUserDto: CreateUserDto):Promise<User>{
         const {email, password, username} = createUserDto;
         const user = this.create({email: email, password:password, username: username});
+       
+        const findEmail = await this.findOne({email:email});
+        if(findEmail){
+            throw new ConflictException('Already Existed email');
+        }
+
+        const findName = await this.findOne({username:username});
+        if(findName){
+            throw new ConflictException('Already Existed username');
+        }
+
+
         await this.save(user);
+       
+
         return user;
     }
 }
